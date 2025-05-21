050028c05a08ed34d8edf95e858d5b65ed4fdb74
BaltiBlast <fougeray.florian@gmail.com>
2025-05-21 18:00:58 +0200

    add express session

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 const express = require("express");
 require("dotenv").config();
+const session = require("express-session");
 const router = require("./router");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const secretSession = process.env.SECRET_SESSION;
 
-app.use(express.json());
-app.use(express.static("public"));
-app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.set("views", "./views");
+app.use(
+  session({
+    secret: secretSession,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
 app.use(router);
 
 app.listen(PORT, () => {
